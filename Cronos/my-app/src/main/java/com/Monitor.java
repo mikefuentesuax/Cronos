64b4a83285b9bc436692c0ea23c8c65247ce6169
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2024-02-24 12:02:13 +0000

    crear variables

--- Cronos/my-app/src/main/java/com/Monitor.java
+++ Cronos/my-app/src/main/java/com/Monitor.java
@@ -3,3 +3,8 @@ package com ;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+public class Monitor {
+    double radiationLevel = 0.0;
+    double temperature = 0.0;
+    double pressure = 0.0;
