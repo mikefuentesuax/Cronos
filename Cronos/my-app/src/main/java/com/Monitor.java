7082e36bd4fcd210a97f3829b2fa7d4e22c12ca1
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2024-02-24 12:02:52 +0000

    condiciones para la temperatura

--- Cronos/my-app/src/main/java/com/Monitor.java
+++ Cronos/my-app/src/main/java/com/Monitor.java
@@ -8,3 +8,11 @@ public class Monitor {
     double radiationLevel = 0.0;
     double temperature = 0.0;
     double pressure = 0.0;
+
+    List<Integer> criticalEvents = new ArrayList<>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29));
+
+    void monitorVariables() {
+        if (radiationLevel > 100 || temperature > 50 || pressure > 1000) {
+            System.out.println("Alert: Critical levels detected");
+        }
+    }
