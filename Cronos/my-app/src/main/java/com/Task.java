f560237981f4d69132ea22e4b9ebb6670f93207c
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2024-02-24 13:44:06 +0000

    problemas y mas problemas

--- Cronos/my-app/src/main/java/com/Task.java
+++ Cronos/my-app/src/main/java/com/Task.java
@@ -1,3 +1,5 @@
+package com;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +12,31 @@ class Task {
         this.name = name;
     }
 
-    
+    int calculateWorkload() {
+        return tasks.stream().mapToInt(task -> task.duration).sum();
+    }
+}
+
+class Scheduler {
+    List<CrewMember> crewMembers = new ArrayList<>();
+
+    void assignTask(Task task, CrewMember crewMember) {
+        task.assignedMember = crewMember;
+        crewMember.tasks.add(task);
+    }
+
+    void displaySchedule() {
+        for (CrewMember crewMember : crewMembers) {
+            System.out.println("Schedule for " + crewMember.name + ":");
+            for (Task task : crewMember.tasks) {
+                System.out.println(task.description + " (" + task.duration + " hours)");
+            }
+        }
+    }
+
+    void adjustSchedule() {
+    }
+}
+
+
 
